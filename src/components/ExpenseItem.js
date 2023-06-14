f1b76106ed972e468e39ab2503038be4938047ca
Chandanindalkar <chandan.s.indalkar@gmail.com>
2023-06-14 15:14:49 +0530

    using props to pass data to components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,17 @@
 import './ExpenseItem.css'
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+    // const expenseDate = new Date(2023, 4, 28);
+    // const expenseTitle = 'Tyre Puncture';
+    // const expenseAmount = Math.random();
+
+
     return (
         <div className="expense-item">
-            <div>Date</div>
+            <div>{props.date.toDateString()}</div>
             <div className="expense-item__description">
-                <h2>Title</h2>
-                <div className="expense-item__price">Amount</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">{props.amount}</div>
             </div>
         </div>
     )
