287516b41690fe7e673f5c30304ccf5a75db6a60
Chandanindalkar <chandan.s.indalkar@gmail.com>
2023-06-20 22:55:52 +0530

    useState to update data in componenets

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,23 @@
+import React, {useState} from 'react';
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate';
 import Card from './Card';
 
 function ExpenseItem(props) {
+    const [title, setTitle] = useState(props.title); 
+    // useState return two values in array, one is data and the other is a function call
+    
+    function clicked() {
+        setTitle('updated!')
+    }
+
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date}/>
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">{props.amount}</div>
+                <button onClick={clicked}>change title</button>
             </div>
         </Card>
     )
