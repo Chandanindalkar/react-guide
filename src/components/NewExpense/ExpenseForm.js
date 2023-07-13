5e3a604843705c36758186b876373b90766ada7c
Chandanindalkar <chandan.s.indalkar@gmail.com>
2023-07-13 18:46:47 +0530

    using a single state, and updating state while considering its previous value

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,23 +2,64 @@ import React, {useState} from 'react';
 import './ExpenseForm.css';
 
 function ExpenseForm() {
-    const [enteredTitle, setenteredTitle] = useState('')
-    const [enteredAmount, setenteredAmount] = useState('')
-    const [enteredDate, setenteredDate] = useState('')
+    // const [enteredTitle, setenteredTitle] = useState('')
+    // const [enteredAmount, setenteredAmount] = useState('')
+    // const [enteredDate, setenteredDate] = useState('')
+    const [userInput, setUserInput] = useState({
+        enteredTitle: '',
+        enteredAmount:'',
+        enteredDate:''
+    })
 
     const titleChangeHandler = (event) => {
-        setenteredTitle(event)
-        setTimeout(console.log(enteredTitle), 5000)
+        // setenteredTitle(event)
+        // setTimeout(console.log(enteredTitle), 5000)
+
+        // setUserInput({
+        //     ...userInput,
+        //     enteredTitle: event.target.value // over writing enteredTitle with userInput Value
+        // })
+        
+        setUserInput((prevState) => {
+            return {
+                ...prevState,
+                enteredTitle: event.target.value
+            };
+        });
     }
 
     const amountChangeHandler = (event) => {
-        setenteredAmount(event.target.value)
-        setTimeout(console.log(enteredAmount), 5000)
+        // setenteredAmount(event.target.value)
+        // setTimeout(console.log(enteredAmount), 5000)
+
+        // setUserInput({
+        //     ...userInput,
+        //     enteredAmount: event.target.value
+        // })
+
+        setUserInput((prevState) => {
+            return {
+                ...prevState,
+                enteredAmount: event.target.value
+            };
+        });        
     }
 
     const dateChangeHandler = (event) => {
-        setenteredDate(event.target.value)
-        setTimeout(console.log(enteredDate), 5000)
+        // setenteredDate(event.target.value)
+        // setTimeout(console.log(enteredDate), 5000)
+
+        // setUserInput({
+        //     ...userInput,
+        //     enteredDate: event.target.value
+        // })
+
+        setUserInput((prevState) => {
+            return {
+                ...prevState,
+                enteredDate: event.target.value
+            };
+        });
     }
 
     return (
