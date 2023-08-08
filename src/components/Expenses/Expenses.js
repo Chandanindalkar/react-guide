20db748df8e0d3b022f6fa322600822024cc39ed
Chandanindalkar <chandan.s.indalkar@gmail.com>
2023-08-09 00:00:00 +0530

    replaced hardcoded rendering with map rendering

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,7 +5,7 @@ import Card from "./Card";
 import ExpensesFilter from "../Filter/ExpensesFilter";
 
 function Expenses(props) {
-    const expenses = props.expense
+    // const expenses = props.expense
 
     const [selectedYear, setSelectedYear] = useState('2023')
 
@@ -20,22 +20,13 @@ function Expenses(props) {
                 onGetSelectedYear = {getSelectedYear}
                 default = {selectedYear}
             />
-
-            <ExpenseItem 
-            title={expenses[0].title}
-            amount = {expenses[0].amount}
-            date = {expenses[0].date}
-            />
-            <ExpenseItem
-            title={expenses[1].title}
-            amount = {expenses[1].amount}
-            date = {expenses[1].date}
-            />
-            <ExpenseItem
-            title={expenses[2].title}
-            amount = {expenses[2].amount}
-            date = {expenses[2].date}
-            />
+            {props.items.map((expense) => (
+                <ExpenseItem>
+                    title={expense.title}
+                    amount = {expense.amount}
+                    date = {expense.date}
+                </ExpenseItem>
+            ))}
         </Card>
     )
 }
